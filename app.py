263b308a5ed7ffa2d426f66ccfa987324dec3f77
kimberlinve <vkimberlin@yahoo.com>
2024-02-26 16:58:40 -0500

    update

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key_here'
 
 # Global variable for weight_input
 weight = None
